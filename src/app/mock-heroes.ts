59c8568eab3b78f9192121e8a281a26e3c89dbae
Dossman-thomas <tdossman.development@gmail.com>
2024-07-23 08:34:18 -0400

    list of seed heroes created to display on the page

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,24 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Spiderman' },
+    { id: 12, name: 'Batman' },
+    { id: 13, name: 'Superman' },
+    { id: 14, name: 'Wonderwoman' },
+    { id: 15, name: 'Ironman' },
+    { id: 16, name: 'Hulk' },
+    { id: 17, name: 'Thor' },
+    { id: 18, name: 'Captain America' },
+    { id: 19, name: 'Black Widow' },
+    { id: 20, name: 'Wolverine' },
+    { id: 21, name: 'Deadpool' },
+    { id: 22, name: 'Doctor Strange' },
+    { id: 23, name: 'Black Panther' },
+    { id: 24, name: 'Antman' },
+    { id: 25, name: 'Hawkeye' },
+    { id: 26, name: 'Vision' },
+    { id: 27, name: 'Scarlet Witch' },
+    { id: 28, name: 'Falcon' },
+    { id: 29, name: 'Winter Soldier' },
+    { id: 30, name: 'War Machine' }
+]; 
